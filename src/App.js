33baa532005073e17f055fbf2a7d6d82dd3a4644
Denisa Ponce <denisaponce2012@hotmail.com>
2022-02-06 21:33:48 -0300

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    document.title = `Has hecho clic ${count} veces`
+  });
+    if (count === 6) {
+    alert ('No se puede sumar más')
+    
+  }
+
+  return (
+    <div className="Container">
+      <div>
+        <h1>Counter App</h1>
+      </div>
+      <div className="Contador ">
+        <button onClick={() => setCount(count + 1)}>
+        + 1
+      </button>
+      <input className="resultado"
+          type='text'
+          placeholder='0'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
 export default App;
